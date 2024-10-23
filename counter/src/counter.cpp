962998d3df98734a4eb39594946bd1e1f1f938fc
PATRICK FENN <patrickfenn@pfenn-macmini.local>
2024-10-22 22:53:52 -0700

    Bug fix for counter.cpp - It will repeatedly reset the counter at midnight regardless of minute

--- counter/src/counter.cpp
+++ counter/src/counter.cpp
@@ -79,9 +79,11 @@ bool Counter::resetTime() {
 
     std::string current_day = days_of_week[local_time->tm_wday];
     int current_hour = local_time->tm_hour;
+    int current_minute = local_time->tm_min;
 
     if (current_day == _dayToReset &&
-        std::to_string(current_hour) == _hourToReset) {
+        std::to_string(current_hour) == _hourToReset &&
+        current_minute == 0) {
 
         return true;
     }
